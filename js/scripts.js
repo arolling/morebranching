98c2fd4c745aead14ca322b34bb85c888d70528f
Abigail Rolling and Jason Wuerch <student@epicodus.com>
2016-01-13 11:38:13 -0800

    final change

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,54 @@ $(document).ready(function() {
 
 
   $("form#chooseme").submit(function(event) {
-    var type = $("select#kingdom").val();
+    var type1 = $("select#kingdom").val();
     var food = $("select#food").val();
 
 
-    if (type === "bird" && food === "carnivore") {
+    if (type1 === "bird" && food === "carnivore") {
       $(".hideme").hide();
       $(".hideme#penguin").show();
-    } else if (type === "bird" && food === "herbivore") {
+    } else if (type1 === "bird" && food === "herbivore") {
       $(".hideme").hide();
       $(".hideme#snowgoose").show();
-    } else if (type === "mammal" && food === "carnivore") {
+    } else if (type1 === "mammal" && food === "carnivore") {
       $(".hideme").hide();
       $(".hideme#bear").show();
-    } else if (type === "mammal" && food === "herbivore") {
+    } else if (type1 === "mammal" && food === "herbivore") {
       $(".hideme").hide();
       $(".hideme#elephant").show();
     }
 
-    if (type) {
+    if (food) {
       $(".moreInfo").show();
     }
     event.preventDefault();
+
+    $("button#moreInfo").click(function() {
+      var type1 = $("select#kingdom").val();
+      var food = $("select#food").val();
+
+
+      if (type1 === "bird" && food === "carnivore") {
+          $("p.hideme").hide();
+          $(".penguinInfo").show();
+        } else if (type1 === "bird" && food === "herbivore") {
+          $("p.hideme").hide();
+          $(".snowgooseInfo").show();
+        } else if (type1 === "mammal" && food === "carnivore") {
+          $("p.hideme").hide();
+          $(".bearInfo").show();
+        } else if (type1 === "mammal" && food === "herbivore") {
+          $("p.hideme").hide();
+          $(".elephantInfo").show();
+
+      };
+
+    });
+
   });
+
+
+
 
 });
